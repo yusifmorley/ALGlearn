bebaf454c1a53b0a193cb99fadd426edacc0c120
morleybread <morleybread@gmail.com>
2022-07-15 16:33:02 +0800

    start

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,16 @@
+
+// 插入排序
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+
+    }
+    public static void sort(int [] list){
+        for (int i = 1; i < list.length; i++) { //从第二位开始遍历
+            int current=list[i];                //
+
+        }
+
+    }
+}
